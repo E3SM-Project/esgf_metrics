6ec9bfb0ca8c6b0fc92fd47b9a808db737de8f18
Tom Vo <tomvothecoder@gmail.com>
2022-07-14 14:17:33 -0700

    Update `settings.py`

--- esgf_metrics/settings.py
+++ esgf_metrics/settings.py
@@ -2,10 +2,9 @@ import os
 
 from dotenv import find_dotenv, load_dotenv
 
-load_dotenv(find_dotenv(".envs/.postgres"), override=True)
+load_dotenv(find_dotenv(), override=True)
 
 # esgf_metrics settings
-load_dotenv(find_dotenv(), override=True)
 LOGS_DIR = os.environ.get("LOGS_DIR", "access_logs")
 OUTPUT_DIR = os.environ.get("OUTPUT_DIR", "output")
 
